630f54d79f7c9153436b3398237fdb10b02f05a1
arijit <arijit100@gmail.com>
2020-07-17 15:24:27 +0100

    updated retirement and working patterns scenarios based on review comments

--- step_definitions/references/manage_references_steps.js
+++ step_definitions/references/manage_references_steps.js
@@ -1,3 +1,6 @@
+var {Then} = require('cucumber');
+var {When} = require('cucumber');
+var {Given} = require('cucumber');
 Given(/^I am on judicial office holder Page$/, function () {
     throw new Error('Not implemented yet');
 });
@@ -47,5 +50,41 @@ Then(/^the judicial office holder can only be assigned to the matching hearing$/
 });
 
 Given(/^the judicial office holder works for 9AM to 2PM on Monday to Thursday$/, function () {
+    throw new Error('Not implemented yet');
+});
+
+Given(/^the User is on create judicial office holder Page$/, function () {
+    throw new Error('Not implemented yet');
+});
+
+Given(/^I add the Retirement Date$/, function () {
+    throw new Error('Not implemented yet');
+});
+
+Given(/^I add the Working Patterns$/, function () {
+    throw new Error('Not implemented yet');
+});
+
+Given(/^judicial office holder record is created$/, function () {
+    throw new Error('Not implemented yet');
+});
+
+Given(/^I update the Retirement Date$/, function () {
+    throw new Error('Not implemented yet');
+});
+
+Given(/^I update the Working Patterns$/, function () {
+    throw new Error('Not implemented yet');
+});
+
+Given(/^the record is updated$/, function () {
+    throw new Error('Not implemented yet');
+});
+
+When(/^I delete the record$/, function () {
+    throw new Error('Not implemented yet');
+});
+
+Then(/^the record is deleted$/, function () {
     throw new Error('Not implemented yet');
 });
