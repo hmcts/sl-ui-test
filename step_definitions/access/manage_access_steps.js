f23d0b61f1a240bb058ea6b956db18b2a7453837
riyan <venkatd648@gmail.com>
2021-03-09 18:50:20 +0000

    updated tests

--- step_definitions/access/manage_access_steps.js
+++ step_definitions/access/manage_access_steps.js
@@ -20,7 +20,6 @@ Given('I submit my login credentials', () => {
 
 Given('I am on my personal dashboard', () => {
   // From "features/access.feature" {"line":10,"column":5}
-  I.seeElement({css: '#menu > li.h6'});
   I.seeElement({ css: 'a[href*="LogoutAction.action"]'});
   I.saveScreenshot("Dashboard_Image.png");
   I.seeVisualDiff("Dashboard_Image.png");
